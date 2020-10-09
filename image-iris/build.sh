e2fd729616b2e2878897a603a006164601f03db1
MC <email>
2020-10-09 14:07:07 -0400

    Some debugging

--- image-iris/build.sh
+++ image-iris/build.sh
@@ -1,3 +1,4 @@
 source ../.env
 
-docker build -t amirsamary/irisdemo:iris.2019.3.0-latest .
+#docker build -t amirsamary/irisdemo:iris.2019.3.0-latest .
+docker build -t mc7913/irisdemo:irisdemo.iko_test01 .
